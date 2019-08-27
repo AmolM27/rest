02fc43c3ced08e6d62714ef24cf6075c7327cec8
DS\mbaxfam4 <amol.mody@manchester.ac.uk>
2019-08-27 09:50:15 +0100

    adding files

---
+++ booksJson.js
@@ -0,0 +1,14 @@
+db.books.insert([
+    {
+        title: 'War and Peace',
+        genre: 'Historical Ficton',
+        author: 'Lev Niko',
+        read: false
+    },
+    {
+        title: 'Les Miserables',
+        genre: 'Historical Ficton',
+        author: 'Victor Hugo',
+        read: false
+    }
+])
